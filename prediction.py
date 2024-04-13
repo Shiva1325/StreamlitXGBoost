33f8cfd537e5df47ecd41aacc83a1b69eaf931e2
Shiva1325 <30314900+Shiva1325@users.noreply.github.com>
2024-04-13 13:49:14 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,6 +1,7 @@
 import joblib as jb
+import pickle
 def predict(data):
-    reg = pickle.load(open('model.pkl','rb'))
+    reg = pickle.load(open('xgb_tune_1.pkl','rb'))
     # reg = jb.load("xgb_tune.sav")
     # return reg.predict(data)
     return 1
