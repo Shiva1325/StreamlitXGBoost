129df709ada48ad6ae29a365b93db3e4cb1dcada
Shiva1325 <30314900+Shiva1325@users.noreply.github.com>
2024-04-13 14:33:50 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,7 +1,8 @@
 import joblib as jb
 import pickle
 def predict(data):
-    print(data['CG_microwave'])
+    
     reg = pickle.load(open('xgb_tune_1.pkl','rb'))
+    print(reg.feature_names_in_)
     # reg = jb.load("xgb_tune.sav")
     return reg.predict(data)
